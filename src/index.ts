3a220fbb2f1e5cc0b4d7a25520872d244fc093d9
Danrley <danrley.dev@gmail.com>
2024-03-16 16:40:20 -0300

    feat: add express and initial server

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import express from 'express';
+import { config } from 'dotenv'
+
+config()
 
 const app = express();
 
+const port = process.env.PORT || 3001;
 
-const port = 3000;
-
-app.listen(3000, ()=> {
+app.listen(port, ()=> {
     console.log(`🔥 Server is running on http://localhost:${port}`);
 });
